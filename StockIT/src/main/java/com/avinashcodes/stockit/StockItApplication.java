4e13f77d41476e6643248f207307431254863d7c
Avinash Kumar <avinash.kumar93801@gmail.com>
2023-07-15 00:19:50 +0530

    First Commit of the repo.

---
+++ StockIT/src/main/java/com/avinashcodes/stockit/StockItApplication.java
@@ -0,0 +1,13 @@
+package com.avinashcodes.stockit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockItApplication.class, args);
+    }
+
+}
